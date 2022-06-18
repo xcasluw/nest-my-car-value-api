1a0969e72901100966b84911bbd81f487555440f
Lucas <oligena@hotmail.com>
2022-06-18 18:34:30 -0300

    added signup, get, getAll and delete endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,28 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param, Query, Delete } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log("🚀 ~ file: users.controller.ts ~ line 8 ~ UsersController ~ createUser ~ body", body)
+    return this.usersService.create(body.email, body.password)
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id))
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email)
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id))
   }
 }
