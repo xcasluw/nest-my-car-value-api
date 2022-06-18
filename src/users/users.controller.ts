ca1898eea2d07019dc441e10d0c37caeb59ca742
Lucas <oligena@hotmail.com>
2022-06-18 17:10:07 -0300

    added new route for create users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    console.log("🚀 ~ file: users.controller.ts ~ line 8 ~ UsersController ~ createUser ~ body", body)
+  }
+}
